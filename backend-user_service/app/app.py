b5146a739a79a882651c40d7a92f225e80ac40c7
Daniel Kamar <kamarster@gmail.com>
2020-11-15 13:46:57 +0300

    chore(heroku-deploy): separate deploy for user microservice

--- backend-user_service/app/app.py
+++ backend-user_service/app/app.py
@@ -70,4 +70,6 @@ def user_loaded_from_header(self, user=None):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    # app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
