1ac93d1697bd2cf3313f100948417d20b4f178e1
Daniel Kamar <kamarster@gmail.com>
2020-11-15 12:24:06 +0300

    chore(refactor-deploy): heroku-dyno-port

--- backend-product_service/app/app.py
+++ backend-product_service/app/app.py
@@ -1,8 +1,8 @@
 import setup
+import os
 
 app = setup.create_app()
 
-port = int(os.environ.get("PORT", 5000))
-
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
